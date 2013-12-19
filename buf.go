c758f93aa6049565f1d338fff8bc96fca27150ca
Mikhail Denisenko <denisenkom@gmail.com>
2013-12-19 00:48:04 -0500

    moved TdsBuffer into buf.go

--- buf.go
+++ buf.go
@@ -1,66 +1,103 @@
 package mssql
 
 import (
-        "bytes"
-        "encoding/binary"
+    "io"
+    "encoding/binary"
 )
 
-type readBuf []byte
-
-func (b *readBuf) int32() (n int) {
-        n = int(int32(binary.BigEndian.Uint32(*b)))
-        *b = (*b)[4:]
-        return
+type TdsBuffer struct {
+    buf []byte
+    pos uint16
+    transport io.ReadWriter
+    size uint16
+    final bool
+    packet_type uint8
 }
 
-func (b *readBuf) int16() (n int) {
-        n = int(binary.BigEndian.Uint16(*b))
-        *b = (*b)[2:]
-        return
+func NewTdsBuffer(bufsize int, transport io.ReadWriter) *TdsBuffer {
+    buf := make([]byte, bufsize)
+    w := new(TdsBuffer)
+    w.buf = buf
+    w.pos = 8
+    w.transport = transport
+    w.size = 0
+    return w
 }
 
-func (b *readBuf) string() string {
-        i := bytes.IndexByte(*b, 0)
-        if i < 0 {
-                //errorf("invalid message format; expected string terminator")
+func (w * TdsBuffer) Write(p []byte) (nn int, err error) {
+    copied := copy(w.buf[w.pos:], p)
+    w.pos += uint16(copied)
+    return copied, nil
+}
+
+func (w * TdsBuffer) WriteByte(b byte) error {
+    w.buf[w.pos] = b
+    w.pos += 1
+    return nil
+}
+
+func (w * TdsBuffer) BeginPacket(packet_type byte) {
+    w.buf[0] = packet_type
+    w.buf[1] = 0  // packet is incomplete
+    w.pos = 8
+}
+
+func (w * TdsBuffer) FinishPacket() error {
+    w.buf[1] = 1  // packet is complete
+    binary.BigEndian.PutUint16(w.buf[2:], w.pos)
+    return WriteAll(w.transport, w.buf[:w.pos])
+}
+
+func (r * TdsBuffer) read_next_packet() error {
+    header := Header{}
+    var err error
+    err = binary.Read(r.transport, binary.BigEndian, &header)
+    if err != nil {
+        return err
+    }
+    offset := uint16(binary.Size(header))
+    _, err = io.ReadFull(r.transport, r.buf[offset:header.Size])
+    if err != nil {
+        return err
+    }
+    r.pos = offset
+    r.size = header.Size
+    r.final = header.Status != 0
+    r.packet_type = header.PacketType
+    return nil
+}
+
+func (r * TdsBuffer) BeginRead() (packet_type uint8, err error) {
+    err = r.read_next_packet()
+    return r.packet_type, err
+}
+
+func (r * TdsBuffer) ReadByte() (res byte, err error) {
+    if r.pos == r.size {
+        if r.final {
+            return 0, io.EOF
         }
-        s := (*b)[:i]
-        *b = (*b)[i+1:]
-        return string(s)
+        err = r.read_next_packet()
+        if err != nil {
+            return 0, err
+        }
+    }
+    res = r.buf[r.pos]
+    r.pos++
+    return res, nil
 }
 
-func (b *readBuf) next(n int) (v []byte) {
-        v = (*b)[:n]
-        *b = (*b)[n:]
-        return
-}
-
-func (b *readBuf) byte() byte {
-        return b.next(1)[0]
-}
-
-type writeBuf []byte
-
-func (b *writeBuf) int32(n int) {
-        x := make([]byte, 4)
-        binary.BigEndian.PutUint32(x, uint32(n))
-        *b = append(*b, x...)
-}
-
-func (b *writeBuf) int16(n int) {
-        x := make([]byte, 2)
-        binary.BigEndian.PutUint16(x, uint16(n))
-        *b = append(*b, x...)
-}
-
-func (b *writeBuf) string(s string) {
-        *b = append(*b, (s + "\000")...)
-}
-
-func (b *writeBuf) byte(c byte) {
-        *b = append(*b, c)
-}
-
-func (b *writeBuf) bytes(v []byte) {
-        *b = append(*b, v...)
+func (r * TdsBuffer) Read(buf []byte) (n int, err error) {
+    if r.pos == r.size {
+        if r.final {
+            return 0, io.EOF
+        }
+        err = r.read_next_packet()
+        if err != nil {
+            return 0, err
+        }
+    }
+    copied := copy(buf, r.buf[r.pos:r.size])
+    r.pos += uint16(copied)
+    return copied, nil
 }
