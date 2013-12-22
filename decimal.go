7ffebc6424c3271925a2158e098a9dc8a74720af
Mikhail Denisenko <denisenkom@gmail.com>
2013-12-21 23:51:03 -0500

    added conversion from float to decimal

--- decimal.go
+++ decimal.go
@@ -1,6 +1,12 @@
 package mssql
 
 
+import (
+    "math"
+    "errors"
+)
+
+
 // http://msdn.microsoft.com/en-us/library/ee780893.aspx
 type Decimal struct {
     integer [4]uint32
@@ -30,8 +36,37 @@ func StrToDecimal(s string) (Decimal, error) {
     return Decimal{}, nil
 }
 
-func Float32ToDecimal(f float32) Decimal {
-    return Decimal{}
+func Float64ToDecimal(f float64) (Decimal, error) {
+    var dec Decimal
+    if math.IsNaN(f) {
+        return dec, errors.New("NaN")
+    }
+    if math.IsInf(f, 0) {
+        return dec, errors.New("Infinity can't be converted to decimal")
+    }
+    dec.positive = f >= 0
+    if !dec.positive {
+        f = math.Abs(f)
+    }
+    if f > 3.402823669209385e+38 {
+        return dec, errors.New("Float value is out of range")
+    }
+    dec.prec = 20
+    var integer float64
+    for dec.scale = 0; dec.scale <= 20; dec.scale++ {
+        integer = f * scaletblflt64[dec.scale]
+        _, frac := math.Modf(integer)
+        if frac == 0 {
+            break
+        }
+    }
+    for i := 0; i < 4; i++ {
+        mod := math.Mod(integer, 0x100000000)
+        integer -= mod
+        integer /= 0x100000000
+        dec.integer[i] = uint32(mod)
+    }
+    return dec, nil
 }
 
 func init() {
