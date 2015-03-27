a4db446a52cdde7e2da5dbdf52f25aa955fb56c3
Linquize <linquize@yahoo.com.hk>
2015-03-27 22:19:14 +0800

    Conditionally compile with net.Dialer.KeepAlive option if go >= 1.3

--- tds.go
+++ tds.go
@@ -741,7 +741,7 @@ func connect(params map[string]string) (res *tdsSession, err error) {
 		return nil, err
 	}
 	addr := p.host + ":" + strconv.FormatUint(p.port, 10)
-	d := net.Dialer{Timeout: p.dial_timeout, KeepAlive: p.keepAlive}
+	d := createDialer(p)
 	conn, err := d.Dial("tcp", addr)
 	if err != nil {
 		f := "Unable to open tcp connection with host '%v': %v"
