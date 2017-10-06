ee80a5a2c2456b397d5080eb71e03a3586c24f54
forfuncsake <forfuncsake@gmail.com>
2017-10-06 11:59:22 +1100

    Add support for typeInt1 and typeInt2 in switch statements (#299)
    Fixes #298

--- types_test.go
+++ types_test.go
@@ -25,4 +25,16 @@ func TestMakeGoLangScanType(t *testing.T) {
 	if (reflect.TypeOf(time.Time{}) != makeGoLangScanType(typeInfo{TypeId: typeDateTim4})) {
 		t.Errorf("invalid type returned for typeDateTim4")
 	}
+	if (reflect.TypeOf(int64(0)) != makeGoLangScanType(typeInfo{TypeId: typeInt1})) {
+		t.Errorf("invalid type returned for typeInt1")
+	}
+	if (reflect.TypeOf(int64(0)) != makeGoLangScanType(typeInfo{TypeId: typeInt2})) {
+		t.Errorf("invalid type returned for typeInt2")
+	}
+	if (reflect.TypeOf(int64(0)) != makeGoLangScanType(typeInfo{TypeId: typeInt4})) {
+		t.Errorf("invalid type returned for typeInt4")
+	}
+	if (reflect.TypeOf(int64(0)) != makeGoLangScanType(typeInfo{TypeId: typeIntN, Size: 4})) {
+		t.Errorf("invalid type returned for typeIntN")
+	}
 }
