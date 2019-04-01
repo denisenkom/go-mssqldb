9cb43592707e9bbb758ac6fbba46c6ca20af3a59
v-kaywon <v-kaywon@microsoft.com>
2019-04-01 12:54:54 -0700

    add documentation and example for using tvp type

---
+++ doc/how-to-use-table-valued-parameters.md
@@ -0,0 +1,62 @@
+# How to use Table-Valued Parameters
+
+Table-valued parameters are declared by using user-defined table types. You can use table-valued parameters to send multiple rows of data to a Transact-SQL statement or a routine, such as a stored procedure or function, without creating a temporary table or many parameters.
+
+To make use of the TVP functionality, first you need to create a table type, and a procedure or function to receive data from the table-valued parameter.
+
+```
+createTVP = "CREATE TYPE LocationTableType AS TABLE (LocationName VARCHAR(50), CostRate INT)"
+_, err = db.Exec(createTable)
+
+createProc = `
+CREATE PROCEDURE dbo.usp_InsertProductionLocation
+@TVP LocationTableType READONLY
+AS
+SET NOCOUNT ON
+INSERT INTO Location
+(
+	Name,
+	CostRate,
+    Availability,
+	ModifiedDate)
+SELECT *, 0,GETDATE()
+FROM @TVP`
+_, err = db.Exec(createProc)
+```
+
+In your go application, create a struct that corresponds to the table type you have created. Create a slice of these structs which contain the data you want to pass to the stored procedure.
+
+```
+type LocationTableTvp struct {
+	LocationName string
+	CostRate     int64
+}
+
+locationTableTypeData := []LocationTableTvp{
+	{
+		LocationName: "Alberta",
+		CostRate:     0,
+	},
+	{
+		LocationName: "British Columbia",
+		CostRate:     1,
+	},
+}
+```
+
+Create a `mssql.TVPType` object, and pass the slice of structs into the `TVPValue` member. Set `TVPTypeName` to the table type name, and `TVPScheme` to the Schema which the table type belongs to. If `TVPScheme` is not specified, SQL Server searches the table type from the default schema of your user in SQL Server. Note the `TVPTypeName` and `TVPScheme` members may be combined in future implementations of the go-mssqldb driver (see [issue 458](https://github.com/denisenkom/go-mssqldb/issues/458)).
+
+```
+tvpType := mssql.TVPType{
+	TVPTypeName: "LocationTableType",
+	TVPScheme:   "",
+	TVPValue:    locationTableTypeData,
+}
+```
+
+Finally, execute the stored procedure and pass the `mssql.TVPType` object you have created as a parameter.
+
+`_, err = db.Exec("exec dbo.usp_InsertProductionLocation @TVP;", sql.Named("TVP", tvpType))`
+
+## Example
+[NewConnector example](../tvptype_example_test.go)
---
+++ tvptype_example_test.go
@@ -0,0 +1,105 @@
+// +build go1.10
+
+package mssql_test
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/denisenkom/go-mssqldb"
+)
+
+// This example shows how to use tvp type
+func main() {
+	const (
+		createTable = "CREATE TABLE Location (Name VARCHAR(50), CostRate INT, Availability BIT, ModifiedDate DATETIME2)"
+
+		dropTable = "IF OBJECT_ID('Location', 'U') IS NOT NULL DROP TABLE Location"
+
+		createTVP = `CREATE TYPE LocationTableType AS TABLE
+		(LocationName VARCHAR(50),
+		CostRate INT)`
+
+		dropTVP = "IF type_id('LocationTableType') IS NOT NULL DROP TYPE LocationTableType"
+
+		createProc = `CREATE PROCEDURE dbo.usp_InsertProductionLocation
+		@TVP LocationTableType READONLY
+		AS
+		SET NOCOUNT ON
+		INSERT INTO Location
+		(
+			Name,
+			CostRate,
+			Availability,
+			ModifiedDate)
+			SELECT *, 0,GETDATE()
+			FROM @TVP`
+
+		dropProc = "IF OBJECT_ID('dbo.usp_InsertProductionLocation', 'P') IS NOT NULL DROP PROCEDURE dbo.usp_InsertProductionLocation"
+
+		execTvp = "exec dbo.usp_InsertProductionLocation @TVP;"
+	)
+	type LocationTableTvp struct {
+		LocationName string
+		CostRate     int64
+	}
+
+	flag.Parse()
+
+	if *debug {
+		fmt.Printf(" password:%s\n", *password)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" server:%s\n", *server)
+		fmt.Printf(" user:%s\n", *user)
+	}
+
+	connString := makeConnURL().String()
+	if *debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+
+	db, err := sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	defer db.Close()
+
+	_, err = db.Exec(createTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(createTVP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Exec(dropTVP)
+	_, err = db.Exec(createProc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Exec(dropProc)
+
+	locationTableTypeData := []LocationTableTvp{
+		{
+			LocationName: "Alberta",
+			CostRate:     0,
+		},
+		{
+			LocationName: "British Columbia",
+			CostRate:     1,
+		},
+	}
+
+	tvpType := mssql.TVPType{
+		TVPTypeName: "LocationTableType",
+		TVPScheme:   "",
+		TVPValue:    locationTableTypeData,
+	}
+
+	_, err = db.Exec(execTvp, sql.Named("TVP", tvpType))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
