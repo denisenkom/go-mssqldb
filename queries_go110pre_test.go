44cdfe8d8ba9a7322b38cd8fbb0f2cb445e65a69
Yuki Wong <v-kaywon@microsoft.com>
2019-08-20 15:32:06 -0700

    Disable lastinsertid in go1.10 and later (#510)
    * Update issue templates
    
    * Delete custom.md
    
    * Delete issue-template.md
    
    * Update issue templates
    
    * disable lastinsertid in go1.10 and later

---
+++ queries_go110pre_test.go
@@ -0,0 +1,57 @@
+// +build !go1.10
+
+package mssql
+
+import (
+	"testing"
+)
+
+func TestIdentity(t *testing.T) {
+	conn := open(t)
+	defer conn.Close()
+
+	tx, err := conn.Begin()
+	if err != nil {
+		t.Fatal("Begin tran failed", err)
+	}
+	defer tx.Rollback()
+
+	res, err := tx.Exec("create table #foo (bar int identity, baz int unique)")
+	if err != nil {
+		t.Fatal("create table failed")
+	}
+
+	res, err = tx.Exec("insert into #foo (baz) values (1)")
+	if err != nil {
+		t.Fatal("insert failed")
+	}
+	n, err := res.LastInsertId()
+	if err != nil {
+		t.Fatal("last insert id failed")
+	}
+	if n != 1 {
+		t.Error("Expected 1 for identity, got ", n)
+	}
+
+	res, err = tx.Exec("insert into #foo (baz) values (20)")
+	if err != nil {
+		t.Fatal("insert failed")
+	}
+	n, err = res.LastInsertId()
+	if err != nil {
+		t.Fatal("last insert id failed")
+	}
+	if n != 2 {
+		t.Error("Expected 2 for identity, got ", n)
+	}
+
+	res, err = tx.Exec("insert into #foo (baz) values (1)")
+	if err == nil {
+		t.Fatal("insert should fail")
+	}
+
+	res, err = tx.Exec("insert into #foo (baz) values (?)", 1)
+	if err == nil {
+		t.Fatal("insert should fail")
+	}
+}
