983f955d0a8567669a1e2f2c3f7602cc32841ec8
Thomas Combeléran <55949036+tc-hib@users.noreply.github.com>
2021-07-14 12:39:09 +0200

    Forgot to test connectionGood in the (probably useless) case of mssql.Error

--- mssql.go
+++ mssql.go
@@ -214,11 +214,12 @@ func (c *Conn) checkBadConn(err error, mayRetry bool) error {
 			badConn: mayRetry,
 		}
 	case Error:
-		err.badConn = mayRetry
+		err.badConn = mayRetry && !c.connectionGood
 		return err
 	}
 
 	if mayRetry && !c.connectionGood {
+		fmt.Println("returning ErrBadConn!")
 		return driver.ErrBadConn
 	}
 	return err
